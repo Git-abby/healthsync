9a7073a2f6b891d0463cf5c47dfa08a67b76fd68
Git-abby <malekabrar56@gmail.com>
2024-12-29 03:22:05 -0500

    chore: routes for frontend created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Doctors from './pages/Doctors'
+import MyAppointments from './pages/MyAppointments'
+import Profile from './pages/Profile'
+import Appointments from './pages/Appointments'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className='mx-4 sm:mx-[10%]'>
+<Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/login' element={<Login />} />
+  <Route path='/contact' element={<Contact />} />
+  <Route path='/about' element={<About />} />
+  <Route path='/doctors' element={<Doctors />} />
+  <Route path='/doctors/:speciality' element={<Doctors />} />
+  <Route path='/my-appointments' element={<MyAppointments />} />
+  <Route path='/profile' element={<Profile />} />
+  <Route path='/appointment/:docId' element={<Appointments />} />
+</Routes>
+    </div>
   )
 }
 
