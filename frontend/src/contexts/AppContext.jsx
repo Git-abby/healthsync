eaf22d6ada80b5b720945fe7210823ee1df2320b
Git-abby <malekabrar56@gmail.com>
2025-01-01 22:37:54 -0500

    chore: doctors appointment page(WIP)

--- frontend/src/contexts/AppContext.jsx
+++ frontend/src/contexts/AppContext.jsx
@@ -4,15 +4,17 @@ import { doctors, specialityData } from "../assets/assets";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const currencySymbol = "$";
+
   const value = {
     doctors,
-    specialityData
+    specialityData,
+    currencySymbol,
   };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
 };
-
 
 export default AppContextProvider;
