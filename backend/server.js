f98f57f7897cf96a8db14c28cda8a96198f8f37a
Git-abby <malekabrar56@gmail.com>
2025-01-14 12:20:07 -0500

    chore: backend changes - Server.js

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
-//app config
+//app config 
 const app = express();
 const port = process.env.PORT || 4000;
 
-//middlewares
+//middlewares 
 app.use(express.json());
 app.use(cors());
 
-//API ENDPOINTS
+//API ENDPOINTS 
 app.get("/", (req, res) => {
   res.send(`API WORKING GET`);
 });
