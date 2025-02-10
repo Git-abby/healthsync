38632622f091cbc52c460f8359d63bc656946520
Git-abby <malekabrar56@gmail.com>
2025-02-10 02:39:45 -0500

    fix: changed connection format to connect DB

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,12 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URL}/healthsync`);
+  // await mongoose.connect(`${process.env.MONGODB_URL}/healthsync`);
+  await mongoose.connect(process.env.MONGODB_URL, {
+    dbName: "healthsync",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 export default connectDB;
